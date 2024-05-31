15e1b4cbf86d0c1d1ed23d012e767311a3f9b38b
Sean Youngberg <sean.youngberg@gmail.com>
2024-05-31 04:14:04 -0400

    remove duplicate provider block

--- deployments/iam_roles_cloudngfw/main.tf
+++ deployments/iam_roles_cloudngfw/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.3.0, < 2.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.17"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-2"
-  #profile = "qwiklabs"
-}
-
 ### Get Account ID
 
 data "aws_caller_identity" "current" {}
