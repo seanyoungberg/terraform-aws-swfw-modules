ccb5e8e93d8e69c9d5e40d13bc94b45a997ca30d
Sean Youngberg <sean.youngberg@gmail.com>
2024-05-31 01:03:58 -0400

    testing rules with TF

---
+++ deployments/ql_cloudngfw_isolated_design/rules.tf
@@ -0,0 +1,30 @@
+resource "cloudngfwaws_security_rule" "example" {
+  rulestack   = "terraform-rulestack"
+  rule_list   = "LocalRule"
+  priority    = 3
+  name        = "outbound-app1-vms"
+  description = "Also configured by Terraform"
+  source {
+    prefix_lists = cloudngfwaws_prefix_list.app1_vms
+  }
+  destination {
+    cidrs = ["any"]
+  }
+  negate_destination = true
+  applications       = ["any"]
+  category {}
+  action        = "Allow"
+  logging       = true
+  audit_comment = "initial config"
+}
+
+resource "cloudngfwaws_prefix_list" "app1_vms" {
+  rulestack   = "terraform-rulestack"
+  name        = "tf-prefix-list"
+  description = "App1 VM Subnets"
+  prefix_list = [
+    "10.104.0.0/24",
+    "10.104.128.0/24",
+  ]
+  audit_comment = "initial config"
+}
